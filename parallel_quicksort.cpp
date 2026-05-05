c446e4ee668f7078d6a2b6ffc816c10e7c046bc3
CodeBash265 <prathameshshingade5@gmail.com>
2026-05-05 23:05:48 +0530

    update

---
+++ parallel_quicksort.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+#include <omp.h>
+using namespace std;
+
+// Partition function
+int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+// Parallel quicksort using tasks
+void quickSort(vector<int> &arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        #pragma omp task shared(arr)
+        quickSort(arr, low, pi - 1);
+
+        #pragma omp task shared(arr)
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << "\nOriginal array:\n";
+    for (int x : arr) cout << x << " ";
+    cout << endl;
+
+    double start = omp_get_wtime();
+
+    // Start parallel region
+    #pragma omp parallel
+    {
+        #pragma omp single
+        quickSort(arr, 0, n - 1);
+    }
+
+    double end = omp_get_wtime();
+
+    cout << "\nSorted array:\n";
+    for (int x : arr) cout << x << " ";
+    cout << endl;
+
+    cout << "\nExecution time: " << (end - start) << " seconds\n";
+
+    return 0;
+}
+
+
+/*
+===========================================================
+HOW TO COMPILE AND RUN (OpenMP Parallel Quicksort)
+===========================================================
+
+1. Save the file:
+   Example: parallel_quicksort.cpp
+
+2. Compile the program using g++ with OpenMP support:
+   g++ -fopenmp parallel_quicksort.cpp -o pqsort
+
+3. Run the executable:
+   ./pqsort
+
+4. Provide input when prompted:
+   - Enter number of elements
+   - Enter array elements
+
+-----------------------------------------------------------
+Example Run:
+
+Input:
+Enter number of elements: 5
+Enter elements:
+5 4 3 2 1
+
+Output:
+Original array:
+5 4 3 2 1
+
+Sorted array:
+1 2 3 4 5
+
+Execution time: 0.0001 seconds
+
+-----------------------------------------------------------
+NOTES:
+- The flag -fopenmp enables parallel execution.
+- Make sure your system supports OpenMP.
+- For better performance, use large input sizes.
+- On Mac (if OpenMP not working), install libomp:
+      brew install libomp
+
+===========================================================
+*/
